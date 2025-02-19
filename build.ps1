cfd808d88b11d7d0956b173c970eba4197e71352
Kenji Opdam <kenji.opdam@gmail.com>
2025-02-19 14:25:29 +0100

    fix build script

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 echo "creating virtual environment..."
 # download python installer
-curl https://www.python.org/ftp/python/3.13.2/python-3.13.2-amd64.exe > python-3.13.2-amd64.exe
+Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.13.2/python-3.13.2-amd64.exe -OutFile python-3.13.2-amd64.exe
 # install python 3.13.2 into .\python
 .\python-3.13.2-amd64.exe TargetDir=$PSScriptRoot\python AssociateFiles=0 Shortcuts=0 Include_doc=0 Include_launcher=0 /quiet
 # wait for the python installer to finish
@@ -17,13 +17,13 @@ echo "installing required packages..."
 .\python\Scripts\pip.exe install -r .\requirements.txt --no-warn-script-location
 
 echo "building application..."
-pyinstaller .\main.py --noconfirm --onefile --windowed --icon .\textures\icon.ico
+.\python\Scripts\pyinstaller.exe .\main.py --noconfirm --onefile --windowed --icon .\textures\icon.ico
 
 echo "cleaning up..."
-mv .\dist\main.exe .
-rm -r dist
-rm -r build
-rm -r main.spec
+mv .\dist\main.exe .\autoclicker.exe
+rm -r -Force dist
+rm -r -Force build
+rm -r -Force main.spec
 .\python-3.13.2-amd64.exe /uninstall /quiet
 # wait for the python uninstaller to finish
 while($true) {
@@ -34,5 +34,6 @@ while($true) {
         break
     }
 }
-rm -r python
-rm -r python-3.13.2-amd64.exe
+rm -r -Force python
+rm -r -Force python-3.13.2-amd64.exe
+echo "build finished. resulting binary is autoclicker.exe"
